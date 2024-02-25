39a6f83720a95efaa2019bad0c40f311148e9439
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-25 17:10:52 +0100

    initialize tanstack router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,62 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { Link, Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
-export const Route = createRootRoute({
-	component: () => (
+import { useAuth, type AuthContext } from '../auth';
+
+interface MyRouterContext {
+	auth: AuthContext;
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+	component: RootComponent,
+});
+
+function RootComponent() {
+	const auth = useAuth();
+	return (
 		<>
-			<div className='p-2 flex gap-2'>
-				<Link to='/' className='[&.active]:font-bold'>
+			<div className='p-2 flex gap-2 text-lg'>
+				<Link
+					to='/'
+					activeProps={{
+						className: 'font-bold',
+					}}
+					activeOptions={{ exact: true }}
+				>
 					Home
 				</Link>{' '}
-				<Link to='/about' className='[&.active]:font-bold'>
-					About
-				</Link>
+				{auth.isLoggedIn && !auth.isLoggingOut && (
+					<Link
+						to={'/dashboard'}
+						activeProps={{
+							className: 'font-bold',
+						}}
+					>
+						Dashboard
+					</Link>
+				)}
+				{!auth.isLoggedIn ? (
+					<Link
+						to={'/login'}
+						activeProps={{
+							className: 'font-bold',
+						}}
+					>
+						Login
+					</Link>
+				) : (
+					<button
+						onClick={() => {
+							auth.setIsLoggingOut(true);
+						}}
+					>
+						Logout
+					</button>
+				)}
 			</div>
 			<hr />
 			<Outlet />
-			<TanStackRouterDevtools />
+			<TanStackRouterDevtools position='bottom-right' initialIsOpen={false} />
 		</>
-	),
-});
+	);
+}
