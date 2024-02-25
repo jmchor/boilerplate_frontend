fa1ead1963d2b69f28d5e6e5edfd6e0984fa8bd8
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-25 17:11:54 +0100

    create index lazy loading page

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -2,6 +2,7 @@ import { createLazyFileRoute } from '@tanstack/react-router';
 
 import { useQuery } from '@apollo/client';
 import { graphql } from 'gql.tada';
+import { useState } from 'react';
 
 export const Route = createLazyFileRoute('/')({
 	component: Index,
@@ -16,10 +17,27 @@ const ALL_PROJECTS = graphql(`
 `);
 
 function Index() {
+	const [showProjects, setShowProjects] = useState(false);
 	const { data, error, loading } = useQuery(ALL_PROJECTS);
+
+	const handleShowProjects = () => {
+		setShowProjects(true);
+	};
 
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error</p>;
 
-	return <>{data && data?.allProjects?.map((project) => <p key={project?._id}>{project?.title}</p>)}</>;
+	return (
+		<>
+			{!showProjects && (
+				<div>
+					<p>Welcome! Click here to display all the projects</p>
+					<button onClick={handleShowProjects}>Show Projects</button>
+				</div>
+			)}
+			{showProjects && (
+				<div>{data && data?.allProjects?.map((project) => <p key={project?._id}>{project?.title}</p>)}</div>
+			)}
+		</>
+	);
 }
