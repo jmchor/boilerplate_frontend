05d63131cb37a43e5f1380715b2bdbc91a3841e8
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-28 13:45:41 +0100

    create form styles

---
+++ src/styles/Form.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 20px;
+	font-size: 1.5rem;
+	line-height: 1.5;
+	font-weight: 600;
+	width: 30vw;
+	label {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: 1rem;
+	}
+	input,
+	textarea,
+	select {
+		width: 100%;
+		padding: 0.5rem;
+		font-size: 1.5rem;
+		border: 1px solid black;
+		&:focus {
+			outline: 0;
+			border-color: var(--blue);
+		}
+	}
+	button,
+	input[type='submit'] {
+		width: auto;
+		background: var(--blue);
+		color: white;
+		border: 0;
+		font-size: 2rem;
+		font-weight: 600;
+		padding: 0.5rem 1.2rem;
+	}
+`;
+
+export default Form;
