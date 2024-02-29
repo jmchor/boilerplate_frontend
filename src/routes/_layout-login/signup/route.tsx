a9cd7c2fe695615af3c0fa0b9407a43e03ea1483
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-29 14:38:18 +0100

    Update Signup route

--- src/routes/_layout-login/signup/route.tsx
+++ src/routes/_layout-login/signup/route.tsx
@@ -1,13 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router';
+import SignupForm from '../../../components/SignupForm';
 
 export const Route = createFileRoute('/_layout-login/signup')({
 	component: Signup,
 });
 
 function Signup() {
-	return (
-		<>
-			<p>Sign Up</p>
-		</>
-	);
+	return <SignupForm />;
 }
