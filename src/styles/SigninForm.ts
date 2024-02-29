d67c7f6d2654bb20d910f56f7e66826e85e2b1ab
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-29 14:38:31 +0100

    Externalize Signin Wrapper

---
+++ src/styles/SigninForm.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SigninFormWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+`;
