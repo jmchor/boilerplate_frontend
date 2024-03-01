9e0e3795195621ac27c1f83cccb7b02d3a3d70c5
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-01 17:22:55 +0100

    create function for article gql query

---
+++ src/services/getArticles.ts
@@ -0,0 +1,33 @@
+import { useQuery } from '@apollo/client';
+import { graphql } from 'gql.tada';
+
+const All_ARTICLES_QUERY = graphql(`
+	query Articles {
+		allArticles {
+			title
+			subheadline
+			text
+			tags
+			imageUrl
+			externalLink
+			linkedProjects {
+				_id
+				title
+			}
+			createdBy {
+				username
+				_id
+			}
+			_id
+			createdAt
+		}
+	}
+`);
+
+const useGetArticles = () => {
+	const { data, error, loading } = useQuery(All_ARTICLES_QUERY);
+
+	return { data, error, loading };
+};
+
+export default useGetArticles;
