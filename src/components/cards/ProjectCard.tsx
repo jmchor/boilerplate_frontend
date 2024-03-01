cf90b191245885e05067c8b2875683c43fa3ec6c
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-01 17:20:16 +0100

    update project card component

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -1,52 +1,5 @@
 import { Link } from '@tanstack/react-router';
-import styled from 'styled-components';
-
-const CardContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-around;
-	align-items: center;
-	width: 270px;
-	height: 220px;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	padding: 15px;
-	background-color: #f5f5f5;
-	box-shadow: 0 12px 24px 0 #4f7bc8ba;
-
-	&:hover {
-		box-shadow: 0 12px 24px 0 #4f7bc8fa;
-	}
-`;
-
-const MetaDataWrapper = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-around;
-	align-items: center;
-	width: 80%;
-	gap: 5px;
-	padding: 5px;
-`;
-
-const ToolImage = styled.img`
-	width: 4.5rem;
-	height: 4.5rem;
-	padding: 2px;
-`;
-
-const MetaData = styled.div`
-	h5,
-	p {
-		margin: 0;
-	}
-`;
-
-const Title = styled.div`
-	h3 {
-		margin: 0;
-	}
-`;
+import { CardContainer, MetaData, MetaDataWrapper, Title, ToolImage } from '../../styles/ProjectCardStyles';
 
 const ProjectCard = ({ project }: any) => {
 	function truncateText(text: string, maxLength: number): string {
