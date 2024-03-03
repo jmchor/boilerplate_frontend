260923f831430944b87925905d90834934a57fa0
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-03 09:06:57 +0100

    add color variables

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -11,19 +11,24 @@ const GlobalStyles = createGlobalStyle`
 
 html {
 	--red: #ff0000;
-	--blue: #4F7BC8;
+	--blue: #346fa1;
 	--black: #393939;
 	--purple: #6F2DBD;
 	--mauve: #A663CC;
 	--wisteria: #B298DC;
 	--blueish: #B8D0EB;
-	--celeste: #B9FAF8
+	--celeste: #B9FAF8;
+	--steelbluegrey: linear-gradient(90deg, hsla(213, 77%, 14%, 1) 0%, hsla(202, 27%, 45%, 1) 100%);
+	--Reversesteelbluegrey: linear-gradient(60deg, hsla(202, 27%, 45%, 1) 0%, hsla(213, 77%, 14%, 1) 120%);
+	--radiantBluegrey: radial-gradient(circle, hsla(202, 27%, 45%, 1) 0%, hsla(213, 77%, 14%, 1) 100%);
 	--grey: #3A3A3A;
 	--gray: var(--grey);
+	--darkbluegray: hsla(213, 77%, 14%, 1);
+	--bluegrey: hsla(202, 27%, 45%, 1);
 	--lightblue: #8FB1EC;
 	--lightGrey: #e1e1e1;
-	--lightGray: var(--lightGrey)
-	--offWhite: #ededed;
+	--lightGray: var(--lightGrey);
+	--offWhite: #f5f5f5;
 	--maxwidth: 1000px;
 	--bs: 0 12px 24px 0 rgba(0,0,0,0.09);
 	box-sizing: border-box;
@@ -41,6 +46,7 @@ body {
 	margin: 0;
 	font-size: 1.5rem;
 	line-height: 2;
+	/* background: var(--offWhite); */
 }
 
 a {
