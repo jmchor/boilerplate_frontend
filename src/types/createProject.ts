30baf37aeb2725fcb2cc7c4a04aa03d1e27a2b5f
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-05 14:21:28 +0100

    create types

---
+++ src/types/createProject.ts
@@ -0,0 +1,13 @@
+export type FrontendFramework = 'reactts' | 'reactjs' | 'vanillajs' | 'nextjs' | null | undefined;
+
+export type BackendEnvironment = 'nodets' | 'nodejs' | 'nodeExpressTS' | 'nodeExpressJS' | null | undefined;
+
+export type Modules = 'commonjs' | 'module' | null | undefined;
+
+export type Cms = 'keystoneJS' | 'strapi' | null | undefined;
+
+export type Database = 'mongodb' | 'postgres' | null | undefined;
+
+// export type FrontendPackages = 'jsonwebtoken' | null | undefined;
+
+export type BackendPackages = 'jsonwebtoken' | 'cors' | 'bcryptjs' | 'dotenv' | 'nodemon';
