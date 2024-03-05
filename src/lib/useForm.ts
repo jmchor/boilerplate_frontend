790be67fd9520294e58177ab4fd8c96b6aa8bdaf
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-05 14:10:24 +0100

    update useForm

--- src/lib/useForm.ts
+++ src/lib/useForm.ts
@@ -1,31 +1,39 @@
 import { useEffect, useState } from 'react';
 
-interface Inputs {
-	username: string;
-	email: string;
-	password: string;
+// Define a generic type for inputs
+export interface Inputs {
+	[key: string]: string | number | boolean | string[] | null | object; // You can adjust this type definition as needed
 }
 
-export default function useForm(initial: Inputs = { username: '', email: '', password: '' }) {
-	//create a state object for our inputs
-
-	const [inputs, setInputs] = useState(initial);
+export default function useForm<T extends Inputs>(initial: T) {
+	// Create a state object for our inputs
+	const [inputs, setInputs] = useState<T>(initial);
 	const initialValues = Object.values(initial).join('');
 
 	useEffect(() => {
-		//This function runs when the things we are watching change
+		// This function runs when the things we are watching change
 		setInputs(initial);
 	}, [initialValues]);
 
-	function handleChange(
-		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement | HTMLInputElement>
+	function handleChange<K extends keyof T>(
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
 	) {
 		const { name, type } = e.target;
 
-		const value = type === 'number' ? parseFloat(e.target.value) : e.target.value;
+		let value: string | number | boolean | string[] = e.target.value;
+
+		// Adjust value based on input type
+		if (type === 'number') {
+			value = parseFloat(value);
+		} else if (type === 'checkbox') {
+			value = (e.target as HTMLInputElement).checked; // Type assertion for checkbox input
+		} else if (type === 'select-multiple') {
+			// Multiple select, gather all selected options' values
+			value = Array.from((e.target as HTMLSelectElement).selectedOptions).map((option) => option.value);
+		}
 
 		setInputs({
-			//copy the existing state
+			// Copy the existing state
 			...inputs,
 			[name]: value,
 		});
@@ -37,11 +45,10 @@ export default function useForm(initial: Inputs = { username: '', email: '', pas
 
 	function clearForm() {
 		const blankState = Object.fromEntries(Object.entries(inputs).map(([key, value]) => [key, '']));
-		setInputs(blankState);
+		setInputs(blankState as T);
 	}
 
-	//return the things we want to surface from this custom hook
-
+	// Return the things we want to surface from this custom hook
 	return {
 		inputs,
 		handleChange,
