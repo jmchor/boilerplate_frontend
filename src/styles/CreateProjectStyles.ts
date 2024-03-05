2e3b2245a61c3b0398d53d6e61e4870fb3ca2d88
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-05 14:21:06 +0100

    externalize createproject styles

---
+++ src/styles/CreateProjectStyles.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import Form from './Form';
+
+export const CreateFormWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 2rem;
+	border-radius: 5px;
+	box-shadow:
+		0 4px 8px 0 rgba(0, 0, 0, 0.2),
+		0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
+
+export const CreateProjectForm = styled(Form)`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 20px;
+	font-size: 1.5rem;
+	line-height: 1.5;
+	font-weight: 600;
+	width: 60vw;
+	label {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 1rem 0;
+		font-size: 1.3rem;
+		word-wrap: break-word;
+	}
+	input,
+	textarea,
+	select {
+		width: 100%;
+		padding: 10px;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+		background-color: #fff;
+		color: #333;
+		font-size: 16px;
+	}
+
+	button,
+	input[type='submit'] {
+		width: auto;
+		background: var(--blue);
+		color: white;
+		border: 0;
+		font-size: 2rem;
+		font-weight: 600;
+		padding: 0.5rem 1.2rem;
+	}
+`;
+
+export const FlexRow = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	width: 100%;
+	flex: 1;
+	gap: 4rem;
+	margin-bottom: 5rem;
+`;
+
+export const FlexColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 50%;
+	margin: 0 1rem 0 0;
+`;
+
+export const CenteredDiv = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh; /* Full viewport height */
+`;
