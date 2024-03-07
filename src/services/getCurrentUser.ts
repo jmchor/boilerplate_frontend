5a75ce1cb095eb63d92710f3a703a2844ee893aa
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-07 07:43:35 +0100

    crate more detailed currentuser query

---
+++ src/services/getCurrentUser.ts
@@ -0,0 +1,46 @@
+import { graphql } from 'gql.tada';
+import { useQuery } from '@apollo/client';
+
+export const CURRENT_USER = graphql(`
+	query CurrentUser {
+		currentUser {
+			username
+			email
+			image
+			_id
+			projects {
+				title
+				_id
+				description
+				frontend {
+					framework
+				}
+				backend {
+					environment
+					gqlServer
+					database
+				}
+			}
+			articles {
+				title
+				_id
+			}
+			likedArticles {
+				title
+				_id
+				createdBy {
+					username
+					_id
+				}
+			}
+		}
+	}
+`);
+
+export const useGetCurrentUser = () => {
+	const { data, error, loading } = useQuery(CURRENT_USER, {
+		fetchPolicy: 'network-only',
+	});
+
+	return { data, error, loading };
+};
