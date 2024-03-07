18e58f4ca457ac551933f1637aa4f7820f6fbc58
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-07 07:43:16 +0100

    export homewrapper style

--- src/routes/_layout-home.tsx
+++ src/routes/_layout-home.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/_layout-home')({
 	component: HomeLayoutComponent,
 });
 
-const HomeWrapper = styled.div`
+export const HomeWrapper = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
