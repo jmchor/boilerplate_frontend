cdb89efd10dbad13211da104cccddddbb2458158
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-13 12:16:32 +0100

    create findproject function

---
+++ src/services/findProject.ts
@@ -0,0 +1,59 @@
+import { useQuery } from '@apollo/client';
+import { graphql } from 'gql.tada';
+
+const FIND_PROJECT_QUERY = graphql(`
+	query FindProject($id: ID) {
+		findProject(_id: $id) {
+			title
+			description
+			frontend {
+				framework
+				gqlClient
+				packages
+			}
+			backend {
+				environment
+				moduleType
+				gqlServer
+				cms
+				packages
+				database
+			}
+			installScripts {
+				frontend
+				backend
+			}
+			kanban {
+				backlog {
+					title
+				}
+				todo {
+					title
+				}
+				doing {
+					title
+				}
+				done {
+					title
+				}
+
+				_id
+			}
+			articles {
+				_id
+			}
+			_id
+			createdBy {
+				_id
+			}
+		}
+	}
+`);
+
+export const useFindProject = (id: string) => {
+	return useQuery(FIND_PROJECT_QUERY, {
+		variables: {
+			id,
+		},
+	});
+};
