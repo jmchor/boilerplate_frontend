63a5a70991f1775f3b31901cba17115b2705e5c9
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-13 12:16:25 +0100

    start project detail page w kanban

--- src/routes/_layout-withAuth/projects/$projectid.tsx
+++ src/routes/_layout-withAuth/projects/$projectid.tsx
@@ -1,5 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useParams } from '@tanstack/react-router';
+import { useFindProject } from '../../../services/findProject';
+import KanbanBoard from '../../../components/Kanban/KanbanBoard';
+import { HomePageWrapper } from '../../../styles/HomeRouteStyles';
 
 export const Route = createFileRoute('/_layout-withAuth/projects/$projectid')({
-	component: () => <div>Hello /projects/$projectid!</div>,
+	component: Project,
 });
+
+function Project() {
+	const projectId = useParams({ from: '/_layout-withAuth/projects/$projectid', select: (p) => p.projectid });
+
+	const { data, loading } = useFindProject(projectId);
+	const kanban = data?.findProject?.kanban;
+
+	return (
+		<HomePageWrapper>
+			<h1>Project</h1>
+			<KanbanBoard kanbanId={kanban?._id} />
+		</HomePageWrapper>
+	);
+}
