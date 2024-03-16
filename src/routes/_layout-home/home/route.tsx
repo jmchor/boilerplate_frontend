b829cfe82fe44aa11a3f992f3ffb9aeae5e7b39b
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-16 14:07:04 +0100

    display searchbar on homepage

--- src/routes/_layout-home/home/route.tsx
+++ src/routes/_layout-home/home/route.tsx
@@ -23,6 +23,7 @@ import {
 
 import { Project } from '../../../types/project.js';
 import { Article } from '../../../types/articles.js';
+import SearchBar from '../../../components/SearchBar.js';
 
 export const Route = createFileRoute('/_layout-home/home')({
 	component: Home,
@@ -38,82 +39,17 @@ const LoadingContainer = styled.div`
 `;
 
 function Home() {
-	const [counts, setCounts] = useState<{ projects: number; articles: number }>({ projects: 2, articles: 3 });
-	const [filter, setFilter] = useState<'projects' | 'articles' | 'all'>('all');
-
 	const auth = useAuth();
-
-	const { data: projectData, error: projectError, loading: projectLoading } = useGetProjects(5);
-	const { data: articleData, error: articleError, loading: articleLoading } = useGetArticles();
-
-	if (projectError || articleError) return <p>{projectError?.message || articleError?.message}</p>;
-
-	const projects = projectData?.allProjects || [];
-	const articles = articleData?.allArticles || [];
-
-	const showMore = (type: 'projects' | 'articles') => {
-		setCounts((prevCounts) => ({ ...prevCounts, [type]: prevCounts[type] + 2 }));
-	};
-
-	const handleFilterChange = (selectedFilter: 'projects' | 'articles' | 'all') => {
-		setFilter(selectedFilter);
-	};
 
 	return (
 		<HomePageWrapper>
-			{projectLoading || articleLoading ? (
-				<LoadingContainer>
-					<MoonLoader color='var(--blue)' />
-				</LoadingContainer>
-			) : null}
-
-			{!projectLoading && !articleLoading && (
-				<FilterContainer>
-					<LinkButton onClick={() => handleFilterChange('all')}>
-						<Image src='/static/infinite.svg' alt='' active={filter === 'all'} />
-					</LinkButton>
-					<LinkButton onClick={() => handleFilterChange('projects')}>
-						<Image src='/static/coding.svg' alt='' active={filter === 'projects'} />
-					</LinkButton>
-					<LinkButton onClick={() => handleFilterChange('articles')}>
-						<Image src='/static/article.svg' alt='' active={filter === 'articles'} />
-					</LinkButton>
-				</FilterContainer>
-			)}
-
-			{!projectLoading && !articleLoading && auth.isLoggedIn ? (
+			{auth.isLoggedIn ? (
 				<OptionalContainer>
 					<h1>Welcome back, {auth.user?.username}!</h1>
 				</OptionalContainer>
 			) : null}
 
-			{(!projectLoading && filter === 'all') || (!projectLoading && filter === 'projects') ? (
-				<ContainerWithHeader>
-					<h2> Explore Recent Projects</h2>
-					<ProjectGrid>
-						{projects.slice(0, counts.projects).map((project) => (
-							<ProjectCard key={project?._id} project={project as Project} />
-						))}
-						{projects.length > counts.projects ? (
-							<MoreButton onClick={() => showMore('projects')}>See More</MoreButton>
-						) : null}
-					</ProjectGrid>
-				</ContainerWithHeader>
-			) : null}
-
-			{(!articleLoading && filter === 'all') || (!articleLoading && filter === 'articles') ? (
-				<ContainerWithHeader>
-					<h2> Read the Newest Articles</h2>
-					<ArticleContainer>
-						{articles.slice(0, counts.articles).map((article) => (
-							<ArticleCard key={article?._id} article={article as Article} />
-						))}
-						{articles.length > counts.articles ? (
-							<MoreButton onClick={() => showMore('articles')}>See More</MoreButton>
-						) : null}
-					</ArticleContainer>
-				</ContainerWithHeader>
-			) : null}
+			<SearchBar />
 		</HomePageWrapper>
 	);
 }
