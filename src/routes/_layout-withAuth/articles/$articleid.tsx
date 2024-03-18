6a18aaa3d0ee8799c4263d82be5d51e05cec4622
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-18 13:01:57 +0100

    create article detail page

--- src/routes/_layout-withAuth/articles/$articleid.tsx
+++ src/routes/_layout-withAuth/articles/$articleid.tsx
@@ -1,5 +1,115 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useParams } from '@tanstack/react-router';
+import { graphql } from 'gql.tada';
+import styled from 'styled-components';
+import { CenteredDiv } from '../../../styles/CreateProjectStyles';
+import { MoonLoader } from 'react-spinners';
+import { useQuery } from '@apollo/client';
 
 export const Route = createFileRoute('/_layout-withAuth/articles/$articleid')({
-  component: () => <div>Hello /_layout-home/articles/$articleid!</div>
-})
+	component: Article,
+});
+
+const ArticleWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: auto;
+	width: 100%;
+	background-color: white;
+	box-shadow:
+		0 4px 8px 0 rgba(0, 0, 0, 0.2),
+		0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	border-radius: 5px;
+	h1 {
+		margin: 5rem 0 0 0;
+	}
+`;
+
+const FIND_ARTICLE = graphql(`
+	query FIND_ARTICLE($id: ID) {
+		findArticle(_id: $id) {
+			title
+			text
+			subheadline
+			tags
+			imageUrl
+			externalLink
+			linkedProjects {
+				_id
+				title
+			}
+			createdBy {
+				_id
+				username
+			}
+			_id
+		}
+	}
+`);
+
+const Banner = styled.img`
+	width: 100%;
+	height: 30vh;
+	object-fit: cover;
+`;
+
+const ArticleText = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	padding: 0 10rem;
+	text-align: justify;
+	margin-top: 5rem;
+	margin-bottom: 3rem;
+
+	p,
+	h1,
+	h2,
+	h3 {
+		margin: 0;
+	}
+`;
+
+function Article() {
+	const articleId = useParams({ from: '/_layout-withAuth/articles/$articleid', select: (params) => params.articleid });
+
+	const { data, error, loading } = useQuery(FIND_ARTICLE, {
+		variables: {
+			id: articleId,
+		},
+	});
+
+	console.log(data);
+
+	if (loading) {
+		return (
+			<CenteredDiv>
+				<MoonLoader color='var(--blue)' />
+			</CenteredDiv>
+		);
+	}
+
+	if (error) {
+		return (
+			<CenteredDiv>
+				<h1>Error: {error.message}</h1>
+			</CenteredDiv>
+		);
+	}
+	return (
+		<ArticleWrapper>
+			<Banner src={data?.findArticle?.imageUrl} alt='' />
+			<h1>{data?.findArticle?.title}</h1>
+			<p style={{ fontStyle: 'italic', color: 'gray', fontSize: '1.7rem', margin: '0.2rem 0 2rem 0' }}>
+				{data?.findArticle?.subheadline}
+			</p>
+			<p style={{ color: 'gray', fontSize: '1.4rem', margin: '0 0 2rem 0' }}>
+				{' '}
+				by {data?.findArticle?.createdBy?.username}
+			</p>
+			<hr width='80%' />
+			<ArticleText dangerouslySetInnerHTML={{ __html: data?.findArticle?.text }} />
+		</ArticleWrapper>
+	);
+}
