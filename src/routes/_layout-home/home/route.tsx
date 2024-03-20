b89028f0d41b36815a5ef21b4c186dda005dfd47
Johannes Chorzempa <jchorzempa@posteo.org>
2024-03-20 09:16:20 +0100

    first adjustments to unauthenticated home screen

--- src/routes/_layout-home/home/route.tsx
+++ src/routes/_layout-home/home/route.tsx
@@ -4,6 +4,8 @@ import SearchBar from '../../../components/SearchBar.js';
 import { HomePageWrapper, OptionalContainer } from '../../../styles/HomeRouteStyles.js';
 import { useAuth } from '../../../auth.js';
 import styled from 'styled-components';
+import { CenteredDiv } from '../../../styles/CreateProjectStyles.js';
+import { MoonLoader } from 'react-spinners';
 
 export const Route = createFileRoute('/_layout-home/home')({
 	component: Home,
@@ -22,17 +24,26 @@ const CenteredWrapper = styled.div`
 
 function Home() {
 	const auth = useAuth();
+	console.log(auth);
 
-	return (
-		<HomePageWrapper>
-			<CenteredWrapper>
-				{auth.isLoggedIn ? (
+	if (auth.isLoggedIn && auth.user?.username) {
+		return (
+			<HomePageWrapper>
+				<CenteredWrapper>
 					<OptionalContainer>
 						<h1>Welcome back, {auth.user?.username}!</h1>
 					</OptionalContainer>
-				) : null}
-				<SearchBar />
-			</CenteredWrapper>
-		</HomePageWrapper>
-	);
+					<SearchBar />
+				</CenteredWrapper>
+			</HomePageWrapper>
+		);
+	} else {
+		return (
+			<HomePageWrapper>
+				<CenteredWrapper>
+					<SearchBar />
+				</CenteredWrapper>
+			</HomePageWrapper>
+		);
+	}
 }
